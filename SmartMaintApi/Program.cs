36920fcfee8ed9d431e03d2b32fbf3c97f7972ec
Juho Suvela <juho.suvela@suvela-engineering.fi>
2024-05-05 21:22:00 +0300

    Add CORS

--- SmartMaintApi/Program.cs
+++ SmartMaintApi/Program.cs
@@ -4,10 +4,20 @@ using SmartMaintApi.Data;
 using SmartMaintApi.Models;
 using SmartMaintApi.Services;
 
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          policy.WithOrigins("http://localhost:*"); // TO DO: Put maybe to .env file etc
+                      });
+});
 
+// Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddScoped<IUserService, UserService>();
 builder.Services.AddEndpointsApiExplorer();
@@ -23,14 +33,17 @@ builder.Services.AddAuthorizationBuilder();
 // Configure DbContext
 builder.Services.AddDbContext<AppDbContext>(
     options => options.UseNpgsql(builder.Configuration.GetConnectionString("DbConnection")));
+// options => options.UseNpgsql(nvironment.GetEnvironmentVariable("DB_Connection")));
 
 builder.Services.AddIdentityCore<User>()
-    .AddEntityFrameworkStores<AppDbContext>()
-    .AddApiEndpoints();
+   .AddEntityFrameworkStores<AppDbContext>()
+   .AddApiEndpoints(); // Creates automatically endpoints for user account management
 
 var app = builder.Build();
 
-//app.MapIdentityApi<User>(); Creates automatically login, register, etc.
+//app.MapIdentityApi<User>();
+//app.UseAuthorization();
+//app.UseRouting();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -39,7 +52,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors(MyAllowSpecificOrigins);
+
 app.UseHttpsRedirection();
-// app.UseAuthorization();
 app.MapControllers();
 app.Run();
+
